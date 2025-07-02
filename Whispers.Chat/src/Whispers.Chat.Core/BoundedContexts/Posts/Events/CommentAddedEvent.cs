f5b9243a554e83113b78fe253643123f5151428e
CJHarriz <ch1397@gmail.com>
2025-07-03 00:47:23 +0200

    Simple domain events added
    Adding domain events for when the following takes place
    - A user is registered
    - A post/comment is created
    - A post is liked
    - A post is flagged

---
+++ Whispers.Chat/src/Whispers.Chat.Core/BoundedContexts/Posts/Events/CommentAddedEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace Whispers.Chat.Core.BoundedContexts.Posts.Events;
+
+/// <summary>
+/// Event raised when a new comment is added to a post
+/// </summary>
+public class CommentAddedEvent : DomainEventBase
+{
+  public Guid CommentId { get; }
+  public Guid PostId { get; }
+  public Guid AuthorId { get; }
+  public string Content { get; }
+
+  public CommentAddedEvent(Guid commentId, Guid postId, Guid authorId, string content)
+  {
+    CommentId = commentId;
+    PostId = postId;
+    AuthorId = authorId;
+    Content = content;
+  }
+} 
---
+++ Whispers.Chat/src/Whispers.Chat.Core/BoundedContexts/SiteModeration/Events/PostFlaggedEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace Whispers.Chat.Core.BoundedContexts.SiteModeration.Events;
+
+/// <summary>
+/// Event raised when a post is flagged by a moderator
+/// </summary>
+public class PostFlaggedEvent : DomainEventBase
+{
+  public Guid PostId { get; }
+  public Guid ModeratorId { get; }
+  public bool IsMisleading { get; }
+  public bool IsFalseInformation { get; }
+
+  public PostFlaggedEvent(Guid postId, Guid moderatorId, bool isMisleading, bool isFalseInformation)
+  {
+    PostId = postId;
+    ModeratorId = moderatorId;
+    IsMisleading = isMisleading;
+    IsFalseInformation = isFalseInformation;
+  }
+} 
